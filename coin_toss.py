72f42f962a47717871ea929eef68dd3a30267101
Desmazing <dkimodera1@gmail.com>
2024-10-08 19:41:50 +0300

    Updating access to function parameter

--- coin_toss.py
+++ coin_toss.py
@@ -9,19 +9,20 @@ Phase 4: Add-ons
 
 import random
 
+
 def coin_toss(prediction):
     """
     Function to simulate a coin toss bearing either heads or tails as the result
 
-    Args: str; user prrediction of either heads or tails
-    Returns: str; random result of the coin toss
+    Args: str > user prrediction of either heads or tails
+    Returns: str > random result of the coin toss
     """
 
-    user_selection = input("Pick either heads or tails then press enter:\n")
+    prediction = input("Pick either heads or tails then press enter:\n")
     possible_choices = ["HEADS", "TAILS"]
     toss_result = random.choice(possible_choices)
 
-    if user_selection.upper() == toss_result:
+    if prediction.upper() == toss_result:
         print(f"{toss_result.upper()}! You won the flip!")
     else:
         print(f"{toss_result.upper()}! You lose!")
